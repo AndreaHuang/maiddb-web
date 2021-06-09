6621bf845656426f00aad20f3e933b5b75e26f04
Maid DB Dev <maiddb.dev@gmail.com>
2021-06-09 16:33:36 +0800

    update

--- src/pages/navBar.jsx
+++ src/pages/navBar.jsx
@@ -4,18 +4,18 @@ const NavBar = ({ user }) => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-      
-      <Link className="nav-link" to="/"> 
-      <img className="nav-logo" alt="logo" src="../logo.png"/>
-      </Link>
+
+        <Link className="nav-link" to="/">
+          <img className="nav-logo" alt="logo" src="../logo.png" />
+        </Link>
       </div>
 
- 
+
       <div className="navbar-navigation" id="navbarSupportedContent">
         <ul className="navbar-nav">
           <li className="nav-item">
             <Link className="nav-link" to="/cases">
-              Cases
+              Blacklist
             </Link>
           </li>
           <li className="nav-item">
